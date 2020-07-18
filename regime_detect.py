20d43baf7f65b22e755ed67384e495bd6cac1d6b
Yeeellow <65787097+Yeeellow@users.noreply.github.com>
2020-07-18 02:27:08 -0400

    Update regime_detect.py

--- regime_detect.py
+++ regime_detect.py
@@ -6,6 +6,8 @@ import yfinance as yf
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator
 from sklearn.decomposition import PCA
+from pypfopt import EfficientFrontier
+from pypfopt import objective_functions
 
 
 class HMM:
@@ -160,68 +162,216 @@ class Portfolio:
     Portfolio is a class defining the characteristics of a portfolio
     """
 
-    def __init__(self, assets_names):
-        """
+    def __init__(self, asset_price, tickers):
+        '''
         Initialize class attributes
-        :param assets_names: A list of assets' names
-        """
-        self.assets_names = assets_names
-        self.weights = []
-        self.assets_data = []
+        :param assets_price: dataframe including trading universe and regime
+        :param tickers: asset tickers
+        '''
+        
+        self.tickers = tickers
+        self.price = asset_price
+        self.ret = None #daily log return
+        
+        self.weights_special = [] #optimum weights for regime-based portfolio
+        self.weights_base = [] #optimum weights for base portfolio
+        
+        
+        self.date0 = None #dates for regime 0
+        self.date1 = None #dates for regime 1
+        
+        self.mu_special = None #expected return for special portfolio
+        self.S_special = None #covariance matrix for special portfolio
+        
+        self.mu_base = None #expected return for base portfolio
+        self.S_base = None #expected return for base portfolio
+        
+        self.value_special = 0 #special portfolio value
+        self.value_base = 0 #base portfolio value
 
-    def absorption_ratio(self, data, is_returns=False, lookback=500, halflife=250, num_pc=5):
-        """
-        Calculate the absorption ratio from
-        :param data:
-        :param is_returns:
-        :param lookback:
-        :param halflife:
-        :param num_pc:
-        :return:
-        """
-        if is_returns:
-            ret = data
+    def initialize_ret_cov(self,n=500):
+        '''
+        Initialize expected returns and covariance matrices
+        :param n: first n days for initializing return and cov
+        '''
+        self.ret = np.log(self.price.iloc[:,:-1]).diff().dropna()
+        state = self.price['regime'][1:]
+        df = self.ret[:n]
+        #expected returns and covariance matrix for base portfolio
+        self.S_base = df.ewm(span=len(df),min_periods=len(df)).cov().dropna() * 252
+        self.mu_base = df.ewm(span=len(df),min_periods=len(df)).mean().iloc[-1] * 252
+        #expected returns and covariance matrix for special portfolio
+        self.date0 = list(state[:n][state == 0].index)
+        self.date1 = list(state[:n][state == 1].index)
+        if state[n] == 0:
+            df = df.loc[self.date0]
         else:
-            ret = np.log(df_price).diff().dropna()
+            df = df.loc[self.date1]
+        self.S_special = df.ewm(span=len(df),min_periods=len(df)).cov().dropna() * 252
+        self.mu_special = df.ewm(span=len(df),min_periods=len(df)).mean().iloc[-1] * 252 
+    
+    def UpdateRetCov(self,state):
+        '''
+        Update expected returns and covairance matrices for base and special portfolios
+        :param state: today's regime: 0 or 1
+        '''
+        #special portfolio
+        if state == 0:
+            span = len(self.date0)
+            self.mu_special = self.ret.loc[self.date0].ewm(span=span,min_periods=span).mean().iloc[-1] * 252
+            self.S_special = self.ret.loc[self.date0].ewm(span=span,min_periods=span).cov().dropna() * 252
+        elif state == 1:
+            span = len(self.date1)
+            self.mu_special = self.ret.loc[self.date1].ewm(span=span,min_periods=span).mean().iloc[-1] * 252
+            self.S_special = self.ret.loc[self.date1].ewm(span=span,min_periods=span).cov().dropna() * 252
+        #base portfolio
+        span = len(self.date0) + len(self.date1)
+        date = sorted(self.date0 + self.date1)
+        self.mu_base = self.ret.loc[date].ewm(span=span,min_periods=span).mean().iloc[-1] * 252
+        self.S_base = self.ret.loc[date].ewm(span=span,min_periods=span).cov().dropna() * 252
+            
+    def to_list(self,dic):
+        '''
+        convert a OrderedDict into a list
+        :param dic: ordered dictionary
+        '''
+        return [dic[i] for i in self.tickers]
+    
+    def OptimizeWeight(self,n=500):
+        '''
+        Choose optimum weights for each day
+        :param n: start computing weights after n days
+        '''
+        state = self.price['regime'][1:] 
+        #initialize expected returns and covariance matrix
+        self.initialize_ret_cov(n)
+        #determine first weights for special portfolio
+        ef = EfficientFrontier(self.mu_special, self.S_special, weight_bounds=(-1,1))
+        ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+        ef.max_sharpe(risk_free_rate=0.02)
+        #special weight
+        special = to_list(ef.clean_weights())
+        self.weights_special.append(special)
+        #determine first weights for baseline portfolio
+        ef = EfficientFrontier(self.mu_base, self.S_base, weight_bounds=(-1,1))
+        ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+        ef.max_sharpe(risk_free_rate=0.02)
+        #base weight
+        base = to_list(ef.clean_weights())
+        self.weights_base.append(base)
+        for i,t in zip(range(n,len(state)),self.ret.index[n:]):
+            if state[i] == 0:
+                self.date0.append(t)
+            else:
+                self.date1.append(t)
+            
+            #no regime shift
+            if state[i] == state[i-1]:
+                self.weights_special.append(special)
+                self.weights_base.append(base)
+            #regime shift
+            else:
+                self.UpdateRetCov(state=state[i])
+                #update weights for special portfolio
+                ef = EfficientFrontier(self.mu_special, self.S_special, weight_bounds=(-1,1))
+                ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+                ef.max_sharpe(risk_free_rate=0.02)
+                special = to_list(ef.clean_weights())
+                #update weights for baseline portfolio
+                ef = EfficientFrontier(self.mu_base, self.S_base, weight_bounds=(-1,1))
+                ef.add_objective(objective_functions.L2_reg, gamma=0.1)
+                ef.max_sharpe(risk_free_rate=0.02)
+                base = to_list(ef.clean_weights())
+                #store weights
+                self.weights_special.append(special)     
+                self.weights_base.append(base)  
+        
+        date = self.ret.index[n-1:]
+        self.weights_special = pd.DataFrame(self.weights_special,columns=self.tickers,index=date)
+        self.weights_base = pd.DataFrame(self.weights_special,columns=self.tickers,index=date)
 
-        ar = []
-        for i in range(lookback, len(ret) + 1):
-            model = PCA(n_components=num_pc).fit(ret[i - lookback:i])
-            pc = pd.DataFrame(model.transform(ret))
-            # variance explained by first n principal components
-            pc_var = pc.ewm(halflife=halflife, min_periods=lookback).var().sum().sum()
-            # variance in original data
-            ret_var = ret.ewm(halflife=halflife, min_periods=lookback).var().sum().sum()
-            # absorption ratio
-            ar.append(pc_var / ret_var)
-
-        ar = pd.DataFrame(ar)
-        ar.index = ret.index[lookback - 1:]
-        ar.columns = ['AR']
-
-        return ar
-
-    def optimize(self):
-        return
+    def Portfolio(self,capital=1e6,rebalance=60):
+        '''
+        Compute special/base portfolio value for each day
+        :param capital: starting capital to invest
+        :param rebalance: rebalance frequency in days
+        '''
+        #compute optimum weights
+        self.OptimizeWeight(n=500)
+        #retrieve price and regime data
+        price = self.price.loc[self.weights_special.index].drop('regime',axis=1)
+        regime = self.price.loc[self.weights_special.index]['regime']
+        #portfolio values
+        special = []
+        base = []
+        for i,t in zip(range(len(price)),price.index):
+            #when regime shifts, reallocate capital
+            if i > 0 and regime.iloc[i] != regime.iloc[i-1]:
+                holdings1 = special[-1] * self.weights_special.loc[t] / price.loc[t]
+                holdings2 = base[-1] * self.weights_base.loc[t] / price.loc[t]
+            
+            #rebalance the portoflios regularly
+            elif i > 0 and i % rebalance == 0:
+                holdings1 = special[-1] * self.weights_special.loc[t] / price.loc[t]
+                holdings2 = base[-1] * self.weights_base.loc[t] / price.loc[t]
+            
+            elif i == 0:
+                holdings1 = capital * self.weights_special.loc[t] / price.loc[t]
+                holdings2 = capital * self.weights_base.loc[t] / price.loc[t]
+            
+            value1 = np.sum(holdings1 * price.loc[t]) #special
+            value2 = np.sum(holdings2 * price.loc[t]) #base
+            
+            special.append(value1)
+            base.append(value2)
+        
+        self.value_special = pd.DataFrame(special,columns=['value'],index=price.index)
+        self.value_base = pd.DataFrame(special,columns=['value'],index=price.index)
 
     def visualization(self):
-        return
+        '''
+        Plot and compare special and base portoflios
+        
+        '''
+        df = pd.DataFrame()
+        df['special'] = self.value_special['value']
+        df['base'] = self.value_base['value']
+        df.plot()
 
-    def sharpe_ratio(self, rp, rf, days=252):
-        """
-        Calculate the sharpe ratio of the portfolio
-        :param rp: Daily return of the portfolio
-        :param rf: Risk free rate
-        :param days: Time span
-        :return: Sharpe ratio of the portfolio
-        :rtype: float
-        """
-        volatility = rp.std() * np.sqrt(days)
-        sr = (rp.mean() - rf) / volatility
-        return sr
 
-    def metric(self):
-        return
+    def metric(self,portfolio):
+        '''
+        Evaluate portfolio performance
+        :param portfolio: dataframe of portfolio value
+        
+        return annual return, volatility, Sharpe ratio, Maximum Drawdown
+        '''
+        df = portfolio.copy()
+        df['year'] = [df.index[i].year for i in range(len(df))]
+        metric = pd.DataFrame()
+        
+        #annualized return
+        annual_ret = (df.groupby('year').last() - df.groupby('year').first())/df.groupby('year').first()
+        days = df.groupby('year').count()
+        annual_ret = (1 + annual_ret)**(252/days) - 1
+        metric = annual_ret
+        metric['days'] = days
+        metric.columns = ['annual return','days']
+        
+        #annualilzed volatility
+        df['daily return'] = df['value'].pct_change()
+        metric['daily vol'] = df.groupby('year')[['daily return']].std()
+        metric['annual vol'] = metric['daily vol'] * np.sqrt(metric['days'])
+        
+        #sharpe ratio
+        metric['Sharpe'] = metric['annual return']/metric['annual vol']
+        
+        #MDD
+        df['cummax'] = df.groupby('year')['value'].cummax()
+        df['DD'] = (df['value'] - df['cummax'])/df['cummax']
+        metric['MDD'] = df.groupby('year')['DD'].min()
+        
+        return metric[['annual return','annual vol','Sharpe','MDD']]
 
 
 if __name__ == '__main__':
